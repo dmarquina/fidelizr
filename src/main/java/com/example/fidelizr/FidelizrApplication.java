d3fc10a3b218fe9f243c55a6458f87b7618b2cc7
dmarquina <mrdiego0892@gmail.com>
2024-03-20 11:33:29 -0500

    initial

---
+++ src/main/java/com/example/fidelizr/FidelizrApplication.java
@@ -0,0 +1,13 @@
+package com.example.fidelizr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FidelizrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FidelizrApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/fidelizr/controller/UserRestController.java
@@ -0,0 +1,29 @@
+package com.example.fidelizr.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags = "Users")
+@RestController
+@RequestMapping("/users")
+public class UserRestController {
+
+        @ApiOperation(value = "Listar plantas por usuario",
+                notes = "Servicio para listar las plantas por usuario")
+        @ApiResponses(value = { @ApiResponse(code = 201, message = "Plantas listadas correctamente"),
+                @ApiResponse(code = 400, message = "Solicitud inválida"),
+                @ApiResponse(code = 500, message = "Error en el servidor") })
+        @GetMapping(value = "/{id}/plants", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
+                produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+        public ResponseEntity<String> createUser() {
+            return ResponseEntity.ok("");
+        }
+}
